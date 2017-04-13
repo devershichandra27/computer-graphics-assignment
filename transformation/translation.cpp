5959bc4b9af1abb29db7701ef2a18888a152be5c
Devershi Chandra <devershichandra27@gmail.com>
2017-04-13 12:32:02 +0530

    Added scaling and menu option

--- transformation/translation.cpp
+++ transformation/translation.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 using namespace std;
 
+bool scaleFlag = false, rotationFlag = false, translateFlag = false, reflectionFlag = false;
+
 struct Point
 {
 	GLint x;
@@ -24,6 +26,21 @@ struct Triangle
 	//Point a4 ;
 };
 
+Point a1 = {15, 25, 0};
+Point a2 = {115, 125, 0};
+Point a3 = {215, 25, 0};
+Triangle originalTriangle = {a1, a2, a3};
+
+enum MenuItems
+{
+	TRANSLATE,
+	ROTATION,
+	REFLECTION,
+	SCALE,
+};
+
+MenuItems show = TRANSLATE;
+
 
 void matrixMultiplication(GLfloat a[3], GLfloat b[3][3], GLfloat prod[3])
 {
@@ -40,6 +57,7 @@ void drawTriangle(Triangle originalTriangle)
 		glVertex2f(originalTriangle.a3.x, originalTriangle.a3.y);
 		glVertex2f(originalTriangle.a1.x, originalTriangle.a1.y);
 	glEnd();
+	glFlush();
 }
 
 
@@ -54,7 +72,6 @@ Triangle translate(Triangle inputTriangle, GLint dx, GLint dy)
 	//GLfloat oldVertexVectorA4[3] = {inputTriangle.a4.x, inputTriangle.a4.y, 1};
 	
 	GLfloat translationMatrix[3][3] = {{1,0,0}, {0,1,0}, {dx, dy, 1}};
-
 	//matrixMultiplication(oldVertexVectorA4, translationMatrix, newVertexVectorA4);
 	matrixMultiplication(oldVertexVectorA3, translationMatrix, newVertexVectorA3);
 	matrixMultiplication(oldVertexVectorA2, translationMatrix, newVertexVectorA2);
@@ -102,7 +119,6 @@ Triangle rotate(Triangle inputTriangle, GLfloat angle)
 	Triangle finalTriangle = translate(rotatedTriangle, -x, y);
 	// cout << "New Cordinates "<<newTriangle.a1.x << " " << newTriangle.a4.y <<endl;
 	return finalTriangle;	
-
 }
 
 Triangle reflection(Triangle inputTriangle, Line referneceLine)
@@ -125,6 +141,32 @@ Triangle reflection(Triangle inputTriangle, Line referneceLine)
 	return finalTriangle;
 }
 
+Triangle scaling(Triangle inputTriangle, GLfloat sX, GLfloat sY)
+{
+	GLint x = -40, y=-30;
+	Triangle shiftedTirangle = translate(inputTriangle, x, y);
+	//drawTriangle(shiftedTirangle);
+	GLfloat newVertexVectorA1[3], newVertexVectorA2[3], newVertexVectorA3[3];
+
+	GLfloat oldVertexVectorA1[3] = {shiftedTirangle.a1.x, shiftedTirangle.a1.y, 1};
+	GLfloat oldVertexVectorA2[3] = {shiftedTirangle.a2.x, shiftedTirangle.a2.y, 1};
+	GLfloat oldVertexVectorA3[3] = {shiftedTirangle.a3.x, shiftedTirangle.a3.y, 1};
+	
+	GLfloat scalingMatrix[3][3] = {{sX, 0, 0}, {0,sY, 0}, {0, 0, 1}};
+
+	matrixMultiplication(oldVertexVectorA3, scalingMatrix, newVertexVectorA3);
+	matrixMultiplication(oldVertexVectorA2, scalingMatrix, newVertexVectorA2);
+	matrixMultiplication(oldVertexVectorA1, scalingMatrix, newVertexVectorA1);
+
+	Point newA3 = {newVertexVectorA3[0], newVertexVectorA3[1], 1};
+	Point newA2 = {newVertexVectorA2[0], newVertexVectorA2[1], 1};
+	Point newA1 = {newVertexVectorA1[0], newVertexVectorA1[1], 1};
+
+	Triangle scaledTriangle = {newA1, newA2, newA3};
+
+	Triangle finalTriangle = translate(scaledTriangle, -x, -y);
+	return finalTriangle;
+}
 
 
 
@@ -132,29 +174,72 @@ void displayOriginal()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(0.0, 0.0,0.0);
-/*	cout << originalTriangle.a1.x << " " << originalTriangle.a1.y <<endl;
-	cout << originalTriangle.a2.x << " " << originalTriangle.a2.y <<endl;
-	cout << originalTriangle.a3.x << " " << originalTriangle.a3.y <<endl;
-	cout << originalTriangle.a4.x << " " << originalTriangle.a4.y <<endl;*/
-	Point a1 = {25, 25, 0};
-	Point a2 = {125, 125, 0};
-	Point a3 = {225, 25, 0};
-	//Point a4 = {125, -75, 0};
-	Triangle originalTriangle = {a1, a2, a3};
 	drawTriangle(originalTriangle);
-	int x =-200; int y = -40;
-	Triangle translatedTriangle = translate(originalTriangle, x, y);
-	GLfloat PI_4 = 3.14/4;
-	Triangle rotatedTriangle = rotate(originalTriangle, PI_4 );
-	 drawTriangle(rotatedTriangle);
-	drawTriangle(translatedTriangle);
-	Line l = {{0, 0}, {1,1}};
-	Triangle reflectedTriangle = reflection(originalTriangle, l);
-	drawTriangle(reflectedTriangle);
 	glFlush();
 }
 
+void onMouseClick(int a, int b, int c, int d)
+{
+	if(translateFlag)
+	{
 
+		int x =-200; int y = -40;
+		Triangle translatedTriangle = translate(originalTriangle, x, y);
+		drawTriangle(translatedTriangle);
+		translateFlag = false;
+	}
+	if(reflectionFlag)
+	{
+		Line l = {{0, 0}, {1,1}};
+		Triangle reflectedTriangle = reflection(originalTriangle, l);
+		drawTriangle(reflectedTriangle);
+		reflectionFlag = false;	
+	}
+	if(rotationFlag)
+	{
+		GLfloat PI_4 = 3.14/4;
+		Triangle rotatedTriangle = rotate(originalTriangle, PI_4 );
+		drawTriangle(rotatedTriangle);
+		rotationFlag = false;
+	}
+	if(scaleFlag)
+	{
+		Triangle scaledTriangle = scaling(originalTriangle, 1.5,1.5);
+		drawTriangle(scaledTriangle);
+		scaleFlag = false;
+	}
+
+}
+
+static void menu (int item)
+{
+	switch(item)
+	{
+		case TRANSLATE:
+		{
+			translateFlag = true;
+			break;
+		}
+
+		case REFLECTION:
+		{
+			reflectionFlag = true;
+			break;
+		}
+
+		case ROTATION:
+		{
+			rotationFlag = true;
+			break;
+		}
+		case SCALE:
+		{
+			scaleFlag = true;
+			break;
+		}
+	}
+	glutPostRedisplay();
+}
 
 void init()
 {
@@ -173,7 +258,13 @@ int main(int argc, char *argv[])
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Transformation");
 	init();
-
+	glutMouseFunc(onMouseClick);
+	glutCreateMenu(menu);
+		glutAddMenuEntry("Translate", TRANSLATE);
+		glutAddMenuEntry("Rotate", ROTATION);
+		glutAddMenuEntry("Reflection", REFLECTION);
+		glutAddMenuEntry("Scale", SCALE);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 	glutDisplayFunc(displayOriginal);
 	glutMainLoop();
 	return 0;
